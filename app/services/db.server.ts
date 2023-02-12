3bf890ca0d7f8a3fceef25f765c8f72924da65e1
Henrik Cooke <henrik@cooke.se>
2023-02-12 10:02:21 +0100

    Add db logging

--- app/services/db.server.ts
+++ app/services/db.server.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { logger } from "logger";
 import env from "~/services/env.server";
 
 let db: PrismaClient;
@@ -11,12 +12,46 @@ declare global {
 // the server with every change, but we want to make sure we don't
 // create a new connection to the DB with every change either.
 if (env.NODE_ENV === "production") {
-  db = new PrismaClient();
+  db = createPrismaClient();
 } else {
   if (!global.__db) {
-    global.__db = new PrismaClient();
+    global.__db = createPrismaClient();
   }
   db = global.__db;
+}
+
+function createPrismaClient() {
+  const client = new PrismaClient({
+    log: [
+      {
+        emit: "event",
+        level: "query",
+      },
+      {
+        emit: "stdout",
+        level: "error",
+      },
+      {
+        emit: "stdout",
+        level: "info",
+      },
+      {
+        emit: "stdout",
+        level: "warn",
+      },
+    ],
+  });
+
+  client.$on("query", (e) => {
+    logger.info(
+      "Db query: %s (params: %s) (%d ms)",
+      e.query,
+      e.params,
+      e.duration
+    );
+  });
+
+  return client;
 }
 
 export { db };
