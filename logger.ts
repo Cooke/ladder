3224fc8a84280c03880462646d048fc628f26494
Henrik Cooke <henrik@cooke.se>
2023-02-12 18:51:26 +0100

    Include debug

--- logger.ts
+++ logger.ts
@@ -2,12 +2,12 @@ import pino from "pino";
 import { env } from "./app/services/env.server";
 import pinoToSeq from "pino-seq";
 
-let transport;
+let stream;
 if (env.SEQ_SERVER) {
-  transport = pinoToSeq.createStream({
+  stream = pinoToSeq.createStream({
     serverUrl: env.SEQ_SERVER,
     apiKey: env.SEQ_API_KEY,
   });
 }
 
-export const logger = pino(transport);
+export const logger = pino({ level: "debug" }, stream ?? process.stdout);
