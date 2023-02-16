8aedf446511fd8ea1526aa0b7ac56e31a45212bf
Henrik Cooke <henrik@cooke.se>
2023-02-16 19:48:24 +0100

    Trying new remix config

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: "vercel",
+  //serverBuildTarget: "vercel",
   // When running locally in development mode, we use the built in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
@@ -9,7 +9,8 @@ module.exports = {
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "api/index.js",
+  serverBuildPath: "api/index.js",
   // publicPath: "/build/",
   serverDependenciesToBundle: ["nanoid"],
+  serverPlatform: "node",
 };
